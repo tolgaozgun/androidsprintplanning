30279a65fd43424573f215e4b498602764bfb2da
Tolga Özgün <tolgaozgunn@gmail.com>
2022-07-28 14:17:03 +0300

    Commit project files with designs

---
+++ app/src/main/java/com/tolgaozgun/sprintplanning/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tolgaozgun.sprintplanning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tolgaozgun.sprintplanning.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.navigationBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
